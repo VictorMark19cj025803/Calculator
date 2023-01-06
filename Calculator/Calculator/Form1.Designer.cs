c6b5ecc8026e74ac9bf0ff6be62b8ce936726ea1
VictorMark19cj025803 <115849216+VictorMark19cj025803@users.noreply.github.com>
2023-01-06 10:13:24 +0100

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(25, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(385, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // n1
+            // 
+            this.n1.AccessibleName = "n1";
+            this.n1.BackColor = System.Drawing.Color.DarkCyan;
+            this.n1.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n1.Location = new System.Drawing.Point(25, 123);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(71, 43);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.AccessibleName = "";
+            this.n2.BackColor = System.Drawing.Color.DarkCyan;
+            this.n2.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n2.Location = new System.Drawing.Point(123, 123);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(71, 43);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // n3
+            // 
+            this.n3.AccessibleName = "n1";
+            this.n3.BackColor = System.Drawing.Color.DarkCyan;
+            this.n3.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n3.Location = new System.Drawing.Point(217, 122);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(71, 43);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n4
+            // 
+            this.n4.AccessibleName = "n1";
+            this.n4.BackColor = System.Drawing.Color.DarkCyan;
+            this.n4.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n4.Location = new System.Drawing.Point(25, 186);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(71, 43);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.AccessibleName = "n1";
+            this.n5.BackColor = System.Drawing.Color.DarkCyan;
+            this.n5.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n5.Location = new System.Drawing.Point(123, 186);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(71, 43);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.AccessibleName = "n1";
+            this.n6.BackColor = System.Drawing.Color.DarkCyan;
+            this.n6.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n6.Location = new System.Drawing.Point(217, 186);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(71, 43);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n7
+            // 
+            this.n7.AccessibleName = "n1";
+            this.n7.BackColor = System.Drawing.Color.DarkCyan;
+            this.n7.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n7.Location = new System.Drawing.Point(25, 252);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(71, 43);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n8
+            // 
+            this.n8.AccessibleName = "n1";
+            this.n8.BackColor = System.Drawing.Color.DarkCyan;
+            this.n8.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n8.Location = new System.Drawing.Point(123, 252);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(71, 43);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.AccessibleName = "n1";
+            this.n9.BackColor = System.Drawing.Color.DarkCyan;
+            this.n9.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n9.Location = new System.Drawing.Point(217, 252);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(71, 43);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n0
+            // 
+            this.n0.AccessibleName = "n1";
+            this.n0.BackColor = System.Drawing.Color.DarkCyan;
+            this.n0.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n0.Location = new System.Drawing.Point(123, 323);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(71, 43);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // add
+            // 
+            this.add.AccessibleName = "n1";
+            this.add.BackColor = System.Drawing.Color.DarkCyan;
+            this.add.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.add.Location = new System.Drawing.Point(323, 123);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(71, 43);
+            this.add.TabIndex = 11;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // point
+            // 
+            this.point.AccessibleName = "n1";
+            this.point.BackColor = System.Drawing.Color.DarkCyan;
+            this.point.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.point.Location = new System.Drawing.Point(25, 323);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(71, 43);
+            this.point.TabIndex = 12;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = false;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // clear
+            // 
+            this.clear.AccessibleName = "n1";
+            this.clear.BackColor = System.Drawing.Color.DarkCyan;
+            this.clear.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clear.Location = new System.Drawing.Point(217, 323);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(71, 43);
+            this.clear.TabIndex = 13;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // div
+            // 
+            this.div.AccessibleName = "n1";
+            this.div.BackColor = System.Drawing.Color.DarkCyan;
+            this.div.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.div.Location = new System.Drawing.Point(323, 270);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(71, 43);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = false;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // mul
+            // 
+            this.mul.AccessibleName = "n1";
+            this.mul.BackColor = System.Drawing.Color.DarkCyan;
+            this.mul.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mul.Location = new System.Drawing.Point(323, 221);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(71, 43);
+            this.mul.TabIndex = 15;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = false;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // sub
+            // 
+            this.sub.AccessibleName = "n1";
+            this.sub.BackColor = System.Drawing.Color.DarkCyan;
+            this.sub.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sub.Location = new System.Drawing.Point(323, 172);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(71, 43);
+            this.sub.TabIndex = 16;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = false;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // equal
+            // 
+            this.equal.AccessibleName = "n1";
+            this.equal.BackColor = System.Drawing.Color.DarkCyan;
+            this.equal.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.equal.Location = new System.Drawing.Point(323, 319);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(71, 43);
+            this.equal.TabIndex = 17;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = false;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 450);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button n1;
+        private Button n2;
+        private Button n3;
+        private Button n4;
+        private Button n5;
+        private Button n6;
+        private Button n7;
+        private Button n8;
+        private Button n9;
+        private Button n0;
+        private Button add;
+        private Button point;
+        private Button clear;
+        private Button div;
+        private Button mul;
+        private Button sub;
+        private Button equal;
+    }
+}
